3d799655bbb4712a497e6dc0155e7904d8812ab1
SruthiSampath <150772944+SruthiSampath@users.noreply.github.com>
2025-05-27 14:33:45 +0530

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,9 +2,9 @@ const myImage = document.querySelector("img");
 
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "Images/MainImg.png") {
-    myImage.setAttribute("src", "Images/CuteNap.png");
+  if (mySrc === "Images/MainImg.PNG") {
+    myImage.setAttribute("src", "Images/CuteNap.PNG");
   } else {
-    myImage.setAttribute("src", "Images/MainImg.png");
+    myImage.setAttribute("src", "Images/MainImg.PNG");
   }
 });
